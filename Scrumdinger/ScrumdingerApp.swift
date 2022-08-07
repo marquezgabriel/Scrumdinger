14859f756684e0046dcbfc334345ad5991e91843
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-08-07 17:27:13 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Gabriel Marquez on 2022-08-07.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
